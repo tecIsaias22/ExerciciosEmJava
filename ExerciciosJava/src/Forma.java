682929cdfc0053dbf5b23cc0b5539c5a764ce497
isaias <tecnologiaisaias22@gmail.com>
2025-10-01 21:15:15 -0300

    Delete ExerciciosJava directory

--- ExerciciosJava/src/Forma.java
+++
@@ -1,5 +0,0 @@
-// Classe abstrata - serve como "molde" para todas as formas
-abstract class Forma {
-    // Método abstrato: cada forma vai implementar sua própria forma de calcular a área
-    public abstract double calcularArea();
-}
--- ExerciciosJava/src/OlaMundo.java
+++
@@ -1,6 +0,0 @@
-public class OlaMundo {
-    public static void main(String[] args) {
-        String ola = "Ola Mundo Java!";
-        System.out.println(ola);
-    }
-}
